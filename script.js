6d22e381b49cabde2e1eb70ab227026e94a54231
Pierre Trendel <pierre.trendel@gmail.com>
2023-10-05 15:19:17 +0200

    Add a function to display a review and a variable to store the current review index

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const review = {
-  imageUrl: "https://picsum.photos/400",
-  author: "Pierre Trendel",
-  job: "Développeur / Formateur",
-  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. ",
-};
+const reviews = [
+  {
+    imageUrl: "https://picsum.photos/400",
+    author: "Jasmine Albani",
+    job: "Astrophysicienne",
+    description:
+      "Jasmine Albani : Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    imageUrl: "https://picsum.photos/400",
+    author: "Elodie Dupond",
+    job: "Développeuse web",
+    description:
+      "Elodie Dupond : Lorem, ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    author: "Jane Doe",
+    job: "Parachutiste",
+    description:
+      "Jane Doe : Lorem, ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+];
 
 const imageElt = document.getElementById("image");
 const authorElt = document.getElementById("author");
 const jobElt = document.getElementById("job");
 const descriptionElt = document.getElementById("description");
 
-imageElt.src = review.imageUrl;
-authorElt.innerHTML = review.author;
-jobElt.innerHTML = review.job;
-descriptionElt.innerHTML = review.description;
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentReviewIndex = 0;
+
+function displayReview(currentReview) {
+  imageElt.src = currentReview.imageUrl;
+  authorElt.innerHTML = currentReview.author;
+  jobElt.innerHTML = currentReview.job;
+  descriptionElt.innerHTML = currentReview.description;
+}
+
+displayReview(reviews[currentReviewIndex]);
